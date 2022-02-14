740007685b20630a346d2d79b1ed2f12c0fa106c
Vitor Santos <vitor93gs@gmail.com>
2022-02-14 15:07:27 -0300

    inserido o conteudo em páginas e criada a pagina para os filmes com o overview, nome e foto do poster

--- react-movie-app-main/src/components/MovieList.js
+++ react-movie-app-main/src/components/MovieList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieList = (props) => {
 	const FavouriteComponent = props.favouriteComponent;
@@ -10,14 +11,17 @@ const MovieList = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
-					<div
-						onClick={() => props.handleFavouritesClick(movie)}
-						className='overlay d-flex align-items-center justify-content-center'
-					>
-						<FavouriteComponent />
-					</div>
+				<div key={index} className='image-container d-flex justify-content-start m-3'>
+					<Link onClick={() => {props.setPoster(movie.Poster);props.setId(movie.imdbID);}} to={movie.imdbID}>
+						<img src={movie.Poster} alt='movie'/>
+					</Link>
+						<div
+							onClick={() => props.handleFavouritesClick(movie)}
+							className='overlay d-flex align-items-center justify-content-center'
+						>
+							<FavouriteComponent />
+						</div>
+					
 				</div>
 			))}
 		</>
